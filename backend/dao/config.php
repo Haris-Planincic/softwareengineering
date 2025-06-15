553c3efe3ad0e0fcea31ab47493ce206f61963da
harisplanincic <haris.planincic@stu.ibu.edu.ba>
2025-06-16 00:22:25 +0200

    f

--- backend/dao/config.php
+++ backend/dao/config.php
@@ -1,26 +1,24 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL ^ (E_NOTICE | E_DEPRECATED));
 class Config {
-    public static function DB_HOST() {
-        return '127.0.0.1';
-    }
-
-    public static function DB_NAME() {
-        return 'BooknStay';
-    }
-
-    public static function DB_USER() {
-        return 'root';
-    }
-
-    public static function DB_PASSWORD() {
-        return 'haris';
-    }
-
-    public static function JWT_SECRET() {
-        return 'zlatka';
-    }
+   public static function DB_NAME() {
+       return Config::get_env("DB_NAME", "booknstay");
+   }
+   public static function DB_PORT() {
+       return Config::get_env("DB_PORT", 3306);
+   }
+   public static function DB_USER() {
+       return Config::get_env("DB_USER", 'root');
+   }
+   public static function DB_PASSWORD() {
+       return Config::get_env("DB_PASSWORD", 'haris');
+   }
+   public static function DB_HOST() {
+       return Config::get_env("DB_HOST", '127.0.0.1');
+   }
+   public static function JWT_SECRET() {
+       return Config::get_env("JWT_SECRET", 'zlatka');
+   }
+   public static function get_env($name, $default){
+       return isset($_ENV[$name]) && trim($_ENV[$name]) != "" ? $_ENV[$name] : $default;
+   }
 }
-?>
